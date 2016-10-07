dc88fd71760e9a2b6f820738ad495f27c9db6d13
edos4 <edsalinas7b@yahoo.com>
2016-10-08 07:00:48 +0800

    Adds lab6b and lab6c

---
+++ lab6b.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    FILE *fp;
+    fp = fopen("sample.txt", "r");
+    
+    if(!fp){
+            cout << "Cannot open file";
+            system("pause");
+            exit(1);        
+    }
+    char c;
+    while((c=fgetc(fp))!=EOF){
+        cout << c;
+    }    
+    fclose(fp);
+    system("pause");
+    return 0;
+}   
+    
