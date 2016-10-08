a77503f4342202bda41b3696551f6055bcf6b561
Edwin Richbald A. Salinas <edsalinas7b@yahoo.com>
2016-10-08 10:41:32 +0800

    Update hashmat_file.cpp

--- hashmat_file.cpp
+++ hashmat_file.cpp
@@ -5,20 +5,10 @@ using namespace std;
 
 int main(){
 	ifstream f("input.txt");
-	
 	int hashmat, opponent;
-	FILE *fp;
-	fp = fopen ("input.txt", "r");
 	
-	if(!fp){
-		cout << "CANNOT OPEN FILE";
-		system("pause");
-		exit(1);
-	}
 	while (f>>hashmat>>opponent){
-
 	      cout << abs(hashmat-opponent) << endl;
 	}
 	system ("pause > 0");
-	return 0;
 }
